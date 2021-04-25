8fac983de5f17e14e8a297375dfc78990e272197
jhered <56834430+jhered@users.noreply.github.com>
2021-04-25 10:44:53 -0300

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hola Cinty!"}
+		res := &response{Message: "Hola !!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
